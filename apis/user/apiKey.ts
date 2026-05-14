ff086a372ef8f629969d0bc6a3c1e0ebb4aca736
chiskat <1@paperplane.cc>
2026-05-15 03:37:58 +0800

    refactor(auth): 现在默认 api-key 更加明确，创建和查询使用相同的 name

--- apis/user/apiKey.ts
+++ apis/user/apiKey.ts
@@ -2,19 +2,27 @@ import { auth } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
 import { loginProcedure, router } from '@/lib/trpc'
 
+const DEFAULT_API_KEY_NAME = 'DEFAULT_API_KEY'
+
 export const apiKey = router({
   ensure: loginProcedure.query(async ({ ctx }) => {
     const headers = await ctx.getHeaders()
     const { apiKeys } = await auth.api.listApiKeys({ headers })
+    const defaultApiKey = apiKeys.find(apiKey => apiKey.name === DEFAULT_API_KEY_NAME)
 
-    if (apiKeys.length <= 0) {
-      const newApiKey = await auth.api.createApiKey({ body: { name: 'DEFAULT_API_KEY' }, headers })
+    if (!defaultApiKey) {
+      const newApiKey = await auth.api.createApiKey({
+        body: { name: DEFAULT_API_KEY_NAME },
+        headers,
+      })
       const result = { key: newApiKey.key }
 
       return result
     }
 
-    const apiKey = await prisma.apikey.findFirstOrThrow({ where: { id: apiKeys[0].id } })
+    const apiKey = await prisma.apikey.findFirstOrThrow({
+      where: { id: defaultApiKey.id, name: DEFAULT_API_KEY_NAME },
+    })
     const result = { key: apiKey.key }
 
     return result
