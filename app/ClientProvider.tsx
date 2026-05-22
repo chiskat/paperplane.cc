39ca299c2da337baedff48c986e7f48edb59e369
chiskat <1@paperplane.cc>
2026-05-22 13:56:22 +0800

    feat(app): 添加 ark-ui 的 LocaleProvider

--- app/ClientProvider.tsx
+++ app/ClientProvider.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { LocaleProvider } from '@ark-ui/react'
 import { AppProgressProvider } from '@bprogress/next'
 import { AuthQueryProvider } from '@daveyplate/better-auth-tanstack'
 import { QueryClientProvider } from '@tanstack/react-query'
@@ -12,20 +13,22 @@ import { getQueryClient } from '@/lib/query-client'
 import { trpcClientConfig, TRPCProvider } from '@/lib/trpc-client'
 import { replaceEqualDeep } from '@/utils/structural-sharing'
 
-export default function QueryProvider(props: PropsWithChildren) {
+export default function ClientProvider(props: PropsWithChildren) {
   const [queryClient] = useState(() => getQueryClient())
   const [trpcClient] = useState(() => createTRPCClient<AppRouter>(trpcClientConfig))
 
   return (
-    <AppProgressProvider>
-      <QueryClientProvider client={queryClient}>
-        <TRPCProvider trpcClient={trpcClient} queryClient={queryClient}>
-          <AuthQueryProvider queryOptions={{ structuralSharing: replaceEqualDeep }}>
-            {props.children}
-            <Toaster />
-          </AuthQueryProvider>
-        </TRPCProvider>
-      </QueryClientProvider>
-    </AppProgressProvider>
+    <LocaleProvider locale="zh-CN">
+      <AppProgressProvider>
+        <QueryClientProvider client={queryClient}>
+          <TRPCProvider trpcClient={trpcClient} queryClient={queryClient}>
+            <AuthQueryProvider queryOptions={{ structuralSharing: replaceEqualDeep }}>
+              {props.children}
+              <Toaster />
+            </AuthQueryProvider>
+          </TRPCProvider>
+        </QueryClientProvider>
+      </AppProgressProvider>
+    </LocaleProvider>
   )
 }
